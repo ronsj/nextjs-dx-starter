26126da2a0d76320275c36233771df0e1edbb806
Ron San Jose <ronsj1@gmail.com>
2021-03-10 12:37:41 -0800

    Add .tsx example component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Hello from '../components/Hello'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,12 +12,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <Hello name="World" />
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>src/pages/index.tsx</code>
         </p>
       </main>
 
