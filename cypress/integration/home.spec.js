d93ab24f077f61fbbb7c734e4536c5ce00b0840d
Ron San Jose <ronsj1@gmail.com>
2021-03-11 14:40:21 -0800

    Update port

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,7 +2,7 @@
 
 describe('Home', () => {
   it('should display a title', () => {
-    cy.visit('https://localhost:3000')
+    cy.visit('https://localhost:3177')
     cy.get('main > h1').contains('Hello')
   })
 })
