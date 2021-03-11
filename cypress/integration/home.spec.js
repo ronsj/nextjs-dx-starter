36fe7ef387e652f5c293fc417e712a90215ebcf6
Ron San Jose <ronsj1@gmail.com>
2021-03-10 22:09:44 -0800

    Update Cypress 'Home' example

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,7 +2,7 @@
 
 describe('Home', () => {
   it('should display a title', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('https://localhost:3000')
     cy.get('main > h1').contains('Hello')
   })
 })
